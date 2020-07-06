fb7368ca20026651328088bcfc0abe54fa84aae8
Moez B <moezbajwa@hotmail.com>
2020-07-06 14:53:38 -0400

    added server packets

--- src/messaging/server/packets/JoinPacket.java
+++ src/messaging/server/packets/JoinPacket.java
@@ -1,5 +1,12 @@
 package messaging.server.packets;
 
+import java.net.Socket;
+
 public class JoinPacket extends Packet {
 
+	public JoinPacket(Socket socket, String username) {
+		super.socket = socket;
+		super.username = username;
+	}
+	
 }
--- src/messaging/server/packets/MessagePacket.java
+++ src/messaging/server/packets/MessagePacket.java
@@ -1,5 +1,15 @@
 package messaging.server.packets;
 
-public class MessagePacket extends Packet {
+import java.net.Socket;
 
+public class MessagePacket extends Packet {
+	
+	public String message;
+	
+	public MessagePacket(Socket socket, String username, String message) {
+		super.socket = socket;
+		super.username = username;
+		this.message = message;
+	}
+	
 }
